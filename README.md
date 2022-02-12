2e185ba2678fd92fd88b7441f17e7177e57938d1
Woori Han <woori8806@naver.com>
2022-02-12 23:51:44 +0900

    2nd commit: login form

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # momentum_clone_vanilaJS
-momentum clone coding(vanila JS with.nomad coder)
 
-1. 22.02.12 first upload
+## Project
+바닐라JS로만드는 모멘텀(TODO List 웹앱). 
+
+노마드코더의 강의를 보면서 제작합니다. (*노마드코더: https://nomadcoders.co/* )
+
+해당 프로젝트를 통해 JAVASCRIPT의 개념 이해와 숙달하는 것이 목적입니다
+
+추후 동일한 결과를 REACT로 제작하는 계획이 있습니다.
+___
+## Skills
+JAVASCRIPT, HTML, CSS
+
+___
+## 주요 기능
+- Login
+- JS Clock
+- JS To Do List
+- Get Geolocation
+- Get Weather Information
--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
-const loginButton = document.querySelector('#login-form button');
 
-function loginBtn() {
-    console.dir(loginInput);
-    console.log(loginInput.value);
+function onLoginSubmit(event) {
+    event.preventDefault();
+    const userName = loginInput.value;
+    console.log(userName);
 }
-loginButton.addEventListener("click", loginBtn);
+
+loginForm.addEventListener("submit", onLoginSubmit);
