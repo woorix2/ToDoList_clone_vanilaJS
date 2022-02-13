eaf41b0bf2faff3e10162fe94e6fce53676df16f
Woori Han <woori8806@naver.com>
2022-02-13 22:33:15 +0900

    3rd commit: hidden class added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+const HIDDEN_CLASSNAME = 'hidden';
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const userName = loginInput.value;
-    console.log(userName);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
