22ea664b1a4bc69cdd8e7af458162dbc8fff5b20
Woori Han <woori8806@naver.com>
2022-02-14 22:53:40 +0900

    4th commit: save username in local storage

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    const userName = loginInput.value;
+    const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello ${userName}`;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
